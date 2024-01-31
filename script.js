2eca754e1e3648adac464bbb8f87b7e8a6c30811
louie888a <alexlouie@ymail.com>
2024-01-31 17:03:56 +0000

    add citysearch function

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
 const currentDate = dayjs();
 const currentDay = currentDate.format("D/MM/YYYY");
-console.log(currentDay);
-$(".search-button").on("click", function (event) {
-  event.preventDefault();
-  const cityName = $("#search-input").val().trim();
-  const api = "ecab1f42b2ac43189b8225f344642d25";
-  const queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    cityName +
-    "&appid=" +
-    api;
+const searchInput = $("#search-input");
+const searchBtn = $("#search-button");
+const today = $("#today");
+const forecast = $("#forecast");
+const KelToCel = 273.15;
+let cityName;
+let cityBtn;
 
+const api = "ecab1f42b2ac43189b8225f344642d25";
+let queryURL;
+
+function citySearch() {
+  cityName = searchInput.val().trim();
+  if (cityName === "") {
+    alert("Please enter a city name");
+    return;
+  }
+}
+
+searchBtn.on("click", function (event) {
+  event.preventDefault();
+
+  queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${api}`;
   fetch(queryURL)
-    .then((response) => {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      var todayCity = $("<h3>").text(data.city.name);
-      var todayIcon = $("<img>");
-      todayIcon.attr(
+
+      const todayCity = $("<h3>").text(`${data.city.name} (${currentDay})`);
+      const todayIcon = $("<img>").attr(
         "src",
-        "https://openweathermap.org/img/wn/" +
-          data.list[0].weather[0].icon +
-          "@2x.png"
+        `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`
       );
-      var todayTemp = $("<p>").text(
-        "Temperature: " + (data.list[0].main.temp - 273.15).toFixed(0) + "°C"
+      const todayTemp = $("<p>").text(
+        `Temperature: ${(data.list[0].main.temp - KelToCel).toFixed(0)}°C`
       );
-      var todayWind = $("<p>").text("Wind: " + data.list[0].wind.speed + "KPH");
-      var TodayHumidity = $("<p>").text(
-        "Humidity: " + data.list[0].main.humidity + "%"
+      const todayWind = $("<p>").text(`Wind: ${data.list[0].wind.speed}KPH`);
+      const todayHumidity = $("<p>").text(
+        `Humidity: ${data.list[0].main.humidity}%`
       );
-      var todayWeather = $("<div>");
-      todayWeather.append(
+      const todayWeather = $("<div>").append(
         todayCity,
         todayIcon,
         todayTemp,
         todayWind,
-        TodayHumidity
+        todayHumidity
       );
-      $("#today").append(todayWeather);
+      todayWeather.append(todayWeather);
     })
     .catch((error) => {
-      console.log(error);
+      console.log("Error:", error);
     });
 });
